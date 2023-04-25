75e815b925916f0245fa943fac1c42ffbc85b99f
RKostyukov <roma1997sport@mail.ru>
2023-04-25 18:13:59 +0300

    add error handler

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -10,12 +10,17 @@ export const MainPage = () => {
   const [loading, setLoading] = useState(false);
   const [all, setAll] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
+    if (error) setError(false);
     getPerson(currentPage).then(res => {
       if (all === 0) setAll(res.count);
       setData(res.results);
+    }).catch(() => {
+      setError(true);
+      setData([]);
     }).finally(() => setLoading(false));
   }, [currentPage]);
 
@@ -23,7 +28,8 @@ export const MainPage = () => {
     <div className={Styles.personsBlock}>
       {!loading ? data.map(person => <Card key={person.name} person={person}/>) : <Loader/>}
     </div>
-    {!loading && <div className={Styles.pagination}>
+    {error && <p className={Styles.error}>Ошибка загрузки данных :(</p>}
+    {!loading && !error && <div className={Styles.pagination}>
       <Pagination
         variant="outlined"
         color="primary"
